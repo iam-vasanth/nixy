eeab09f08abd547177a8bc835b7e93a833bb40f9
iam-vasant <vk.vasanth.r@gmail.com>
2025-12-22 00:51:14 +0530

    changes to nixy theme

--- flake.nix
+++ flake.nix
@@ -1,47 +1,26 @@
 {
   description = "Nixy Plymouth Theme";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
+    in {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
+        in {
           default = pkgs.stdenvNoCC.mkDerivation {
-            pname = "plymouth-theme-nixy";
+            pname = "plymouth-nixy";
             version = "1.0.0";
-
             src = ./.;
-
-            dontBuild = true;
-
             installPhase = ''
-              INSTALL_PATH="$out/share/plymouth/themes/nixy"
-              mkdir -p $INSTALL_PATH        
-              # Copy all theme files
-              cp -v nixy/nixy.plymouth $INSTALL_PATH/
-              cp -v nixy/nixy.script $INSTALL_PATH/
-              cp -v nixy/logo.png $INSTALL_PATH/
-              
-              # Fix any absolute paths in .plymouth file
-              sed -i "s@/usr/share@$out/share@g" $INSTALL_PATH/nixy.plymouth
-              sed -i "s@/share@$out/share@g" $INSTALL_PATH/nixy.plymouth
+              mkdir -p $out/share/plymouth/themes/nixy
+              cp nixy/* $out/share/plymouth/themes/nixy/
+              sed -i "s|/share|$out/share|g" $out/share/plymouth/themes/nixy/nixy.plymouth
             '';
-
-            meta = with pkgs.lib; {
-              description = "Nixy Plymouth Theme";
-              platforms = platforms.linux;
-              maintainers = [ ];
-            };
           };
         });
     };
